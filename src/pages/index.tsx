73bb619fd1941abadfcbec08edcfab468ef09fca
John Freitas <john.freitasau@gmail.com>
2021-03-31 08:23:17 +1100

    Created main structure page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,24 @@
 import Head from "next/head";
-
+import styles from "./home.module.scss";
 export default function Home() {
   return (
     <>
       <Head>
         <title>igNews | Home</title>
       </Head>
-      <h1>Hello world</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>Hey, welcome 👋</span>
+          <h1>
+            News about the <span>React</span> world.
+            <p>
+              Get access to all the publications <br />
+              <span>for only $9.90 / month</span>
+            </p>
+          </h1>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
