4da9dfd5a8656bc82ecb79c59c3d40b1f6dd3b01
John Freitas <john.freitasau@gmail.com>
2021-04-01 07:15:41 +1100

    Updated landing page to SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import { stripe } from "../services/stripe";
 import { SubscribeButton } from "../SubscribeButton";
@@ -35,8 +35,9 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1Ib8hgBOqMJ1Q63hZIrEQpwL");
+
   // const price = await stripe.prices.retrieve("price_1Ib8hgBOqMJ1Q63hZIrEQpwL", {
   //   expand: ["product"],
   // });
@@ -55,5 +56,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, //24 hours
   };
 };
