1228329ec7ca0aa5e1b76dd4eb65bc91a75a2f07
John Freitas <john.freitasau@gmail.com>
2021-04-01 06:55:41 +1100

    Created SSR with Stripe for the subscription price

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
+import { GetServerSideProps } from "next";
 import Head from "next/head";
+import { stripe } from "../services/stripe";
 import { SubscribeButton } from "../SubscribeButton";
 import styles from "./home.module.scss";
-export default function Home() {
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: string;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -9,14 +19,14 @@ export default function Home() {
       </Head>
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
-          <span>Hey, welcome 👋</span>
+          <span>Hey, welcome 👋 </span>
           <h1>
             News about the <span>React</span> world.
             <p>
               Get access to all the publications <br />
-              <span>for only $9.90 / month</span>
+              <span>for only {product.amount} / month</span>
             </p>
-            <SubscribeButton />
+            <SubscribeButton priceId={product.priceId} />
           </h1>
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
@@ -24,3 +34,26 @@ export default function Home() {
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve("price_1Ib8hgBOqMJ1Q63hZIrEQpwL");
+  // const price = await stripe.prices.retrieve("price_1Ib8hgBOqMJ1Q63hZIrEQpwL", {
+  //   expand: ["product"],
+  // });
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-AU", {
+      style: "currency",
+      currency: "AUD",
+    }).format(price.unit_amount / 100),
+  };
+
+  console.log("product:", product);
+
+  return {
+    props: {
+      product,
+    },
+  };
+};
