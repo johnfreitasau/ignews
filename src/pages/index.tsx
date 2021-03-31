a4aa47a00c0ea6e5e082896c866fed42b3e57065
John Freitas <john.freitasau@gmail.com>
2021-04-01 05:13:42 +1100

    Completed home layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { SubscribeButton } from "../SubscribeButton";
 import styles from "./home.module.scss";
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
               Get access to all the publications <br />
               <span>for only $9.90 / month</span>
             </p>
+            <SubscribeButton />
           </h1>
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
