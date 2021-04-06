0dcff7af9dc66f6c1c8cf9d5b620497396068023
John Freitas <john.freitasau@gmail.com>
2021-04-07 08:14:06 +1000

    Started Prismic implementation

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,4 +1,7 @@
+import Prismic from "@prismicio/client";
+import { GetStaticProps } from "next";
 import Head from "next/head";
+import { getPrismicClient } from "../../services/prismic";
 import styles from "./styles.module.scss";
 
 export default function Posts() {
@@ -38,3 +41,18 @@ export default function Posts() {
     </>
   );
 }
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    [Prismic.predicates.at("document.type", "publication")],
+    {
+      fetch: ["publication.title", "publication.content"],
+      pageSize: 100,
+    }
+  );
+
+  console.log(response);
+
+  return { props: {} };
+};
